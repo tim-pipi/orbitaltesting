eaafee0316a93fbf50337b1d65eb92a26bc40484
Timothy Putra Prasetio <timothypp02@gmail.com>
2023-06-14 16:19:43 +0800

    Add IDL file

---
+++ idl/hello.thrift
@@ -0,0 +1,16 @@
+// idl/hello.thrift
+namespace go hello.example
+
+struct HelloReq {
+    1: required string Name (api.query="name"); // Add api annotations for easier parameter binding
+}
+
+struct HelloResp {
+    1: string RespBody;
+}
+
+
+service HelloService {
+    HelloResp HelloMethod(1: HelloReq request) (api.get="/hello");
+}
+
